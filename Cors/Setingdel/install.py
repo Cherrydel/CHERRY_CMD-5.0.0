c39bd9ed8eae645f32a35425ff5054ea69b2fd5e
CHERRY_DEL <167352914+Cherrydel@users.noreply.github.com>
2024-07-21 16:03:17 +0600

    Delete Cors directory

--- Cors/Setingdel/install.py
+++
@@ -1,4 +0,0 @@
-from colorama import Fore
-import os
-os.system('cls' if os.name == 'nt' else 'clear')
-print (Fore.YELLOW + "python -m pip install tqdm colorama requests pyfiglet py-cpuinfo gputil mysql.connector pymysql sounddevice")
